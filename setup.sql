deedf4a160012ea0be2eca538e39beba180fcb4c
Paid2x <sdtaiga@gmail.com>
2024-10-16 20:01:08 -0700

    Add files via upload

---
+++ setup.sql
@@ -0,0 +1,20 @@
+INSERT INTO task (
+    name,
+    summary,
+    description
+) VALUES
+(
+    "Wash car",
+    "Take the car to the car wash",
+    "Make sure the car gets waxed"
+),
+(
+    "Walk the dog",
+    "Fidio needs his exercise",
+    "Two laps around the park should be enough"
+),
+(
+    "Buy groceries",
+    "Get everything on the list",
+    "We need tomatoes. lettuce, milk and eggs"
+);
